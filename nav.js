fee6abdb1f21769d75e52cb6bd9b2f16f06e3f89
Jason <eurotrucksimulator2.volvo@gmail.com>
2026-07-28 11:16:43 -0700

    tabs for scintilla

--- nav.js
+++ nav.js
@@ -4,6 +4,9 @@
 
   container.innerHTML = `
     <nav>
+      <a href="index.html">
+        <img src="logo.png" alt="Logo" class="logo" width="50" height="24">
+      </a>
       <a href="index.html">Home</a>
       <a href="scintilla.html">Scintilla</a>
       <a href="about.html">About</a>
